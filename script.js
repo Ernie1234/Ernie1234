e236a0a8e1ca184969d62ea1fe98aa66728d5b5e
Ernie <anumahjoshuaeneye@gmail.com>
2022-01-17 03:38:55 +0100

    second commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let darkMode = localStorage.getItem("darkMode");
+
+function enableDarkMode(){
+    document.body.classList.add("darkmode");
+    localStorage.setItem("darkMode", "enabled");
+}
+
+function disabledDarkMode(){
+    document.body.classList.remove("darkmode");
+    localStorage.setItem("darkMode", null);
+}
+if(darkMode === "enabled"){
+    enableDarkMode();
+}
+// Listeners
+const darkModeToggle = document.querySelector("#dark-mode-toggle");
+darkModeToggle.addEventListener("click", ()=>{
+    darkMode = localStorage.getItem("darkMode");
+    darkMode !== "enabled" ? enableDarkMode(): disabledDarkMode;
+});
